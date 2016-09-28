63fa55b2acd4045622602d8da9fbc08e78234268
André Neves <andre@bighuman.com>
2016-09-28 14:12:24 -0400

    More iOS work

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@
 import React from 'react';
 import { Router } from 'react-native-router-flux';
 import scenes from './scenes/scenes';
-import getSceneStyle from './themes/Scenes';
+
+const getSceneStyle = () => ({
+  flex: 1,
+  backgroundColor: '#454545',
+  shadowColor: 'black',
+  shadowOffset: { width: 2, height: 4 },
+  shadowOpacity: 0.5,
+  shadowRadius: 3,
+});
 
 const App = () => (
   <Router
