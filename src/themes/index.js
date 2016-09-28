7efa5677b3ae053760e7600dde57c65e641aff08
André Neves <andre@bighuman.com>
2016-09-28 14:20:38 -0400

    Removing extra Scenes themin

--- src/themes/index.js
+++ src/themes/index.js
@@ -2,12 +2,10 @@ import AppStyles from './AppStyles';
 import Metrics from './Metrics';
 import Colors from './Colors';
 import Images from './Images';
-import Scenes from './Scenes';
 
 export {
   AppStyles,
   Metrics,
   Colors,
-  Images,
-  Scenes
+  Images
 };
